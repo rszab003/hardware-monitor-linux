85674196b0b0bc71b93111ad905c42baac7b6355
Robert Szabo <rszab003@fiu.edu>
2022-02-02 14:01:06 +0100

    clock and temp are logged, made scaffolding for other data

--- src/NvidiaGpuData.py
+++ src/NvidiaGpuData.py
@@ -1,6 +1,4 @@
 import subprocess
-from sys import stdout
-
 
 master = {}
 
@@ -10,12 +8,27 @@ def getGpuTemp():
     # print("HELLO!!! GET THE TEMP!!!")
     temperatureData = subprocess.run(["nvidia-smi", "--query-gpu=temperature.gpu", "--format=csv"], text=True, capture_output=True)
     temperatureData = temperatureData.stdout.split()
-    master[temperatureData[0]] = temperatureData[1]
+    master["GPU Temperature"] = int(temperatureData[1])
     # print(master)
-    # print(temperatureData.stdout)
 
 
 def getGpuClock():
+    global master
+    currClockSpeed = subprocess.run(["nvidia-smi", "--query-gpu=clocks.gr", "--format=csv"], text=True, capture_output=True)
+    currClockSpeed = currClockSpeed.stdout.split("\n")[1][:-4]
+    master["GPU ClockSpeed-MHz"] = int(currClockSpeed)
+    print(master)
+
+
+def getVramUse():
+    pass
+
+
+def getVramClock():
+    pass
+
+
+def getFanSpeed():
     pass
 
 
