d897e28c1c7829f337aa0d8e3109ba845b2d0e2c
Robert Szabo <rszab003@fiu.edu>
2022-02-04 18:37:48 +0100

    reducing scope - Working with Nvidia Drivers only / added Ram Data script

---
+++ src/RamData.py
@@ -0,0 +1,12 @@
+
+
+def getRamData():
+    master = {}
+    with open("/proc/meminfo", "r") as fi:
+        for i in range(0, 10):
+            memData = fi.readline()[:-1]
+            memData = memData.split()
+            if len(memData) != 3: #might not be necessary i just dont know how this differs on other distros
+                raise Exception("ERROR PARSING MEMORY DATA: Check length of /proc/meminfo\nShould be split into three sections.")
+            master[memData[0][:-1]] = memData[1] + " " + memData[2]
+    return master
