0ea8406cd2f56924d5f555adbbd6b6b5a1fc6037
Robert Szabo <rszab003@fiu.edu>
2022-02-19 17:49:33 +0100

    experimenting with curses and asyncio, not quite working yet

--- src/TerminalWindow.py
+++ src/TerminalWindow.py
@@ -1,12 +1,27 @@
-import curses
-import main
+import curses, asyncio
+import main, time
 from curses import textpad
 
+async def updateSimple(sc, xx, highlight):
+    sc.addstr(3,3,str(highlight))
+    navItems = ["CPU", "GPU", "RAM", "MOTHERBOARD"]
+    y,x = sc.getmaxyx()
+    yPlacement = y - 2
+    xPlacement = x - 3
+    totNavLength = 0; count = 0
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    for i in range(0, len(navItems)):
+        totNavLength += len(navItems[i]) + 1
+        if i == highlight:
+            sc.addstr(yPlacement, xPlacement - totNavLength, navItems[i], curses.color_pair(1))
+        else:
+            sc.addstr(yPlacement, xPlacement - totNavLength, navItems[i])   
 
 
-def update(sc, highlight) -> list:
+
+async def update(sc, highlight) -> list:
     programTitle = "OPEN HARDWARE MONITOR"
-    navHelpText = "USE ARROW KEYS TO SWITCH FOCUS"
+    navHelpText = "USE ARROW KEYS"
     y, x = sc.getmaxyx()
     titlePlacement = (x // 2) - (len(programTitle) // 2)
     quitPlacement = y - 2
@@ -17,7 +32,7 @@ def update(sc, highlight) -> list:
     sc.addstr(quitPlacement, 1, "Press \'q\' to quit.")
     sc.addstr(y - 2, navHelpPlacement, navHelpText)
     
-    #BUILD NAV, LOWER RIGHT
+    # BUILD NAV, LOWER RIGHT
     navItems = ["CPU", "GPU", "RAM", "MOTHERBOARD"]
     yPlacement = y - 2
     xPlacement = x - 3
@@ -31,17 +46,13 @@ def update(sc, highlight) -> list:
             sc.addstr(yPlacement, xPlacement - totNavLength, navItems[i])
     return boundingBox
 
-def getHwData(sc):
-    allHwData = main.executeThreads()
-    cpuData = allHwData[0]
-    sc.addstr(2,3, str(cpuData.result()))
 
 
-def run(sc):
+async def run(sc: curses.window):
+    sc.nodelay(True)
     navHighlight = 0
     curses.curs_set(0)
-    # sc.nodelay(1)
-    update(sc, navHighlight)
+    x = 1
     while True:
         key = sc.getch()
         sc.clear()
@@ -56,12 +67,21 @@ def run(sc):
         elif key == curses.KEY_LEFT and navHighlight == 3:
             navHighlight = 0
         # time.sleep(1.5)
-        update(sc, navHighlight)
+        # getHwData(sc, navHighlight)
+        # upd = loop.create_task(update(sc, navHighlight))
+        r1 = asyncio.create_task(updateSimple(sc, x, navHighlight))
+        x += 1
+        await asyncio.sleep(1)
+        # await r1
         sc.refresh()
+        # time.sleep(1)
         
         # getHwData(sc)
         
-
-
-
-curses.wrapper(run)
+if __name__ == "__main__":
+    try:
+        asyncio.run(curses.wrapper(run))
+    except Exception as ex:
+        print(ex)
+    finally:
+        loop.close()
