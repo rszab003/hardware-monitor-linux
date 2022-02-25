e60926dbad646c6e709617ad329efb1254388077
Robert Szabo <rszab003@fiu.edu>
2022-02-25 15:05:54 +0100

    attempting to grab ram config information, but the command requires user password

--- src/parts/RamData.py
+++ src/parts/RamData.py
@@ -1,5 +1,35 @@
-import parts.converter as converter
+import converter
+import subprocess
 
+#reads how many RAM sticks are installed, returns vendor, size, and clock
+def getConfiguration():
+    config = []
+    response = subprocess.Popen(
+        ["sudo", "lshw", "-class", "memory"], stdout=subprocess.PIPE
+        ).communicate()
+    response = response[0].decode("utf-8").split("\n")[20:]
+    cutoff = 0
+    for idx, item in enumerate(response[::-1]):
+        if "*-cache:0" in item:
+            cutoff = len(response) - idx - 1
+            break
+    response = response[:cutoff]
+    response = [res.strip() for res in response]
+    print(response)
+    idx = 0
+
+    #filters out only ram stick information
+    while (idx < len(response)):
+        # print(idx)
+        if "vendor" in response[idx]:
+            for i in range(idx, idx + 7):
+                config.append(response[i])
+            idx += 6
+        idx += 1
+    print("config: {}".format(config))
+
+
+#GETS CURRENT MEMORY USAGE STATUS
 def getRamData() -> dict:
     master = {}
     with open("/proc/meminfo", "r") as fi:
@@ -14,7 +44,10 @@ def getRamData() -> dict:
             master[memData[0][:-1]] = str(newVal) + " " + unit
     
     return master
-    
+
+
 if __name__ == "__main__":
-    master = getRamData()
+    # master = getRamData()
+    # for _ in range(0, 10):
+    master = getConfiguration()
     print(master)
